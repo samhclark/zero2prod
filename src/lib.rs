911ff0a0627d1bc7f2fe9bb0a5ba0d9115d7f07b
Sam Clark <1059176+samhclark@users.noreply.github.com>
2023-08-20 00:02:09 -0500

    Make Clippy happy

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,9 @@ use hyper::server::conn::AddrIncoming;
 
 // No longer `async`
 // Returns longer Server type. Need `hyper` to write these types
+/// # Panics 
+/// TODO    
+#[must_use]
 pub fn run() -> hyper::Server<AddrIncoming, IntoMakeService<Router>> {
     let app = Router::new().route("/health_check", get(|| async {}));
 
